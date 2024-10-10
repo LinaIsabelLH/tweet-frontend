3b0adefb82d7076b3875466cdaaf112b85509ef5
Your name <Your email>
2024-10-10 15:15:24 +0200

    raph

--- components/Login.js
+++ components/Login.js
@@ -2,44 +2,48 @@ import styles from "../styles/Login.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 import { Modal } from "antd";
-import signUp from "./SignUp";
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-
-// import { Modal } from 'antd';
+import SignUp from "./SignUp";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
 
 function Login() {
-
   const [isModalOpen, setIsModalOpen] = useState(false);
   const dispatch = useDispatch();
 
-  const[signUpFirstname, setSignUpFirstname]= useState('');
-  const [signUpUsername, setSignUpUsername] = useState('');
-	const [signUpPassword, setSignUpPassword] = useState('');
+  const [signUpFirstname, setSignUpFirstname] = useState("");
+  const [signUpUsername, setSignUpUsername] = useState("");
+  const [signUpPassword, setSignUpPassword] = useState("");
 
-	const [signInUsername, setSignInUsername] = useState('');
-	const [signInPassword, setSignInPassword] = useState('');
+  const [signInUsername, setSignInUsername] = useState("");
+  const [signInPassword, setSignInPassword] = useState("");
 
-    const signUpModal = () => {
-      setIsModalOpen(true);
-    }
+  const signUpModal = () => {
+    setIsModalOpen(true);
+  };
 
-    const handleRegister = () => {
-      fetch('http://localhost:3000/users/signup', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ firstname: signUpFirstname, username: signUpUsername, password: signUpPassword }),
-      }).then(response => response.json())
-        .then(data => {
-          if (data.result) {
-            dispatch(login({ username: signUpUsername, token: data.token }));
-            setSignUpFirstname('');
-            setSignUpUsername('');
-            setSignUpPassword('');
-            setIsModalOpen(false);
-    } } )};
-  
-    return (
+  const handleRegister = () => {
+    fetch("http://localhost:3000/users/signup", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        firstname: signUpFirstname,
+        username: signUpUsername,
+        password: signUpPassword,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.result) {
+          dispatch(login({ username: signUpUsername, token: data.token }));
+          setSignUpFirstname("");
+          setSignUpUsername("");
+          setSignUpPassword("");
+          setIsModalOpen(false);
+        }
+      });
+  };
+
+  return (
     <div className={styles.main}>
       <div className={styles.login}>
         <FontAwesomeIcon
@@ -48,11 +52,15 @@ function Login() {
           size={"2xl"}
           style={{ color: "#ffffff" }}
         />
-        <h1 className={styles.titre}>See what's <div>happening</div></h1>
+        <h1 className={styles.titre}>
+          See what's <div>happening</div>
+        </h1>
         <h3>Join Hackatweet today.</h3>
-        <button onClick={() => signUpModal()} className={styles.signup}>Sign up</button>
-        <Modal title="Basic Modal" open={isModalOpen} >
-          <signUp handleRegister={handleRegister}/>
+        <button onClick={() => signUpModal()} className={styles.signup}>
+          Sign up
+        </button>
+        <Modal open={isModalOpen}>
+          <SignUp handleRegister={handleRegister} />
         </Modal>
         <h5>Already have an account?</h5>
         <button className={styles.signin}>Sign in</button>
--- components/SignUp.js
+++ components/SignUp.js
@@ -1,18 +1,36 @@
-import { Modal } from 'antd';
-import styles from '../styles/signup.module.css';
+import { Modal } from "antd";
+import styles from "../styles/signup.module.css";
 
-function signUp(){
+function SignUp() {
+  return (
+    <div className={styles.singUpContainer}>
+      <h1>Create your Hackatweet account</h1>
+      <input
+        type="text"
+        placeholder="Firstname"
+        id="signUpFirstname"
+      //  value={signInFirstname}
+        onChange={(e) => setSignUpFirstname(e.target.value)}
+      ></input>
+      <input
+        type="text"
+        placeholder="Username"
+        id="signUpUsername"
+       // value={signInUsername}
+        onChange={(e) => setSignUpUsername(e.target.value)}
+      ></input>
+      <input
+        type="text"
+        placeholder="Password"
+        id="signUpPassword"
+       // value={signInPassword}
+        onChange={(e) => setSignUpPassword(e.target.value)}
+      ></input>
+      <button id="register" onClick={() => handleRegister()}>
+        Sign Up
+      </button>
+    </div>
+  );
+}
 
-    return (
-        <div className={styles.singUpContainer}>
-            <h1>Create your Hackatweet account</h1>
-            <input type="text" placeholder="Firstname" id="signUpFirstname" value={signInFirstname} onChange={(e) => setSignUpFirstname(e.target.value)}></input>
-            <input type="text" placeholder="Username" id="signUpUsername" value={signInUsername} onChange={(e) => setSignUpUsername(e.target.value)}></input>
-            <input type="text" placeholder="Password" id="signUpPassword" value={signInPassword} onChange={(e) => setSignUpPassword(e.target.value)}></input>
-            <button id="register" onClick={() => handleRegister()}>Sign Up</button>
-        </div>
-    )
-
-};
-
-export default signUp;
+export default SignUp;
