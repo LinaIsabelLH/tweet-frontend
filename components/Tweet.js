eabd35512f3bc5bbcfa2d24ba2283d9f33831775
Your name <Your email>
2024-10-10 22:19:42 +0200

    frontend deuxiemepage

--- components/Tweet.js
+++ components/Tweet.js
@@ -1,11 +1,13 @@
 import styles from "../styles/Tweet.module.css";
 
 
+
+
 function Tweet() {
   return (
     <div className={styles.main}>
       <h2>Home</h2>
-      <div className={styles.input}><input className={styles.wazza} placeholder="What's up?" ></input></div>
+      <div className={styles.input}><input className={styles.wazza} maxLength={280} placeholder="What's up?" ></input></div>
       <div className={styles.button}><span>0/280</span><button>Tweet</button></div>
     </div>
   );
