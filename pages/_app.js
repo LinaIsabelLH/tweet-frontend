ad172a81d148123075a47d896f6fcab545141f17
Lina Isabel <londonohidalgo@gmail.com>
2024-10-11 16:00:59 +0200

    front solution

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,11 @@ import Head from 'next/head';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import user from '../reducers/user';
+import tweetRex from '../reducers/tweetRex';
 
 
 const store = configureStore({
-  reducer: {user},
+  reducer: {user, tweetRex},
  });
 
 function App({ Component, pageProps }) {
