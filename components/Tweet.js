2f8783c371b95c0079574c986d7cb3daf2663d60
Your name <Your email>
2024-10-11 13:54:59 +0200

    front

--- components/Tweet.js
+++ components/Tweet.js
@@ -1,14 +1,45 @@
 import styles from "../styles/Tweet.module.css";
-
-
+import { useState } from "react";
 
 
 function Tweet() {
+  
+  const [tweetData, setTweetData] = useState('');
+  
+  
+  let handleClick = () => {
+    fetch("http://localhost:3000/tweets/post", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+          firstname: "Joachim",
+          username: "Jasmin",
+          message: tweetData,
+          date: new Date(),
+      }),
+  })
+      .then((response) => response.json())
+      }
+  ;
+  
+  const length = tweetData.length
+  
   return (
     <div className={styles.main}>
       <h2>Home</h2>
-      <div className={styles.input}><input className={styles.wazza} maxLength={280} placeholder="What's up?" ></input></div>
-      <div className={styles.button}><span>0/280</span><button>Tweet</button></div>
+      <div className={styles.input}>
+        <textarea
+          className={styles.wazza}
+          maxLength={280}
+          placeholder="What's up?"
+          onChange={(e) => setTweetData(e.target.value)}
+          value={tweetData}
+        ></textarea>
+      </div>
+      <div className={styles.button}>
+        <span>{length}/280</span>
+        <button onClick={() => handleClick()}>Tweet</button>
+      </div>
     </div>
   );
 }
